4640cc2f46535253861e8b75285c93008b5fcfd3
Sawaijain <dhariwalsawai@gmail.com>
2023-11-02 22:35:50 +0530

    calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+const CenteredBox = () => {
+  const [inputName, setInputName] = useState('');
+  const [output, setOutput] = useState('');
+
+  const characterToNumberMap = [
+    { characters: 'aijqy', number: '1' },
+    { characters: 'bkr', number: '2' },
+    { characters: 'cgls', number: '3' },
+    { characters: 'dmt', number: '4' },
+    { characters: 'ehnx', number: '5' },
+    { characters: 'uvw', number: '6' },
+    { characters: 'oz', number: '7' },
+    { characters: 'fp', number: '8' },
+  ];
+
+  const handleNameChange = (e) => {
+    setInputName(e.target.value);
+  };
+
+  const reset = ()=>{
+    setInputName("");
+    setOutput("");
+  }
+  const calculateOutput = () => {
+    const name = inputName.toLowerCase();
+    let result = '';
+
+    for (let i = 0; i < name.length; i++) {
+      const char = name[i];
+      const mapping = characterToNumberMap.find(item => item.characters.includes(char));
+      if (mapping) {
+        result += mapping.number;
+      }
+    }
+
+    setOutput(result);
+  };
+
+  const centeredBoxStyles = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+  };
+
+  const buttonContainerStyles = {
+    display: 'flex',
+    flexDirection: 'row', 
+    gap: '10px',
+  };
+
+  const buttonStyles = {
+    backgroundColor: '#0074cc',
+    color: '#fff',
+    border: 'none',
+    padding: '5px 5px',
+    cursor: 'pointer',
+  };
+
+  return (
+    <div style={centeredBoxStyles}>
+      <h1>Name Calculator</h1>
+      <input
+        type="text"
+        placeholder="Enter your name"
+        value={inputName}
+        onChange={handleNameChange}
+        style={{ margin: '10px', padding: '5px' }}
+      />
+      <div style={buttonContainerStyles}>
+        <button onClick={calculateOutput} style={buttonStyles}>
+          Calculate
+        </button>
+        <button onClick={reset} style={buttonStyles}>
+          Reset
+        </button>
+      </div>
+      <p>Output: {output}</p>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <CenteredBox />
     </div>
   );
 }
